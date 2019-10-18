984fac101027a8edfaf5a826b8a5c1a3fe75a0d4
zhaifang <jonathan.zhai@wiwider.com>
2019-10-18 10:20:32 +0800

    new arcface1.0

--- apps/zf.py
+++ apps/zf.py
@@ -2,11 +2,11 @@ import cv2
 import numpy as np
 from models import face_track_server, face_describer_server, face_db, camera_server
 from configs import configs
+import os
 
 '''
 The demo app utilize all servers in model folder with simple business scenario/logics:
 I have a camera product and I need to use it to find all visitors in my store who came here before.
-
 Main logics is in the process function, where you can further customize.
 '''
 
@@ -22,6 +22,22 @@ class Demo(camera_server.CameraServer):
             output_tensor_names=configs.face_describer_output_tensor_names,
             device=configs.face_describer_device)
         self.face_db = face_db.Model()
+        for filename in os.listdir("./pic/"):
+            if filename.endswith(".jpg"):
+                print('add : {}/pic/{}'.format(configs.BASE_PATH, filename))
+                img = cv2.imread('{}/pic/{}'.format(configs.BASE_PATH, filename))
+                self.face_tracker.process(img)
+                db_faces = self.face_tracker.get_faces()
+                print("current FN=", filename)
+                #cv2.imshow("similaire", img_resize)
+                #cv2.waitKey(0)
+                for face in db_faces:
+                    img_resize = cv2.resize(face, configs.face_describer_tensor_shape)
+                    data_feed = [np.expand_dims(img_resize.copy(), axis=0), configs.face_describer_drop_out_rate]
+                    face_description = self.face_describer.inference(data_feed)[0][0]
+                    self.face_db.add_face(img_resize, face_description)
+                    cv2.imshow(filename, img_resize)
+                    cv2.waitKey(100)
 
     def processs(self, frame):
         # Step1. Find and track face (frame ---> [Face_Tracker] ---> Faces Loactions)
@@ -38,16 +54,21 @@ class Demo(camera_server.CameraServer):
         if _num_faces == 0:
             return
         for _face in _faces:
+            #cv2.imshow("similaire test", _face)
+            #cv2.waitKey(0)
             _face_resize = cv2.resize(_face, configs.face_describer_tensor_shape)
-            _data_feed = [np.expand_dims(_face_resize, axis=0), configs.face_describer_drop_out_rate]
+            _data_feed = [np.expand_dims(_face_resize.copy(), axis=0), configs.face_describer_drop_out_rate]
             _face_description = self.face_describer.inference(_data_feed)[0][0]
             _face_descriptions.append(_face_description)
-            print("Face=",_face_description)
+
             # Step3. For each face, check whether there are similar faces and if not save it to db.
             # Below naive and verbose implementation is to tutor you how this work
             _similar_faces = self.face_db.get_similar_faces(_face_description)
-#            if len(_similar_faces) == 0 or len(self.face_db.faces) == 0:
-#                self.face_db.add_face(face_img=_face, face_description=_face_description)
+            for face in _similar_faces:
+                cv2.imshow("similaire", face)
+                cv2.waitKey(1)
+            if len(_similar_faces) == 0 or len(self.face_db.faces) == 0:
+                self.face_db.add_face(face_img=_face, face_description=_face_description)
         print('[Demo] -----------------------------------------------------------')
 
     def _viz_faces(self, faces_loc, frame):
@@ -57,11 +78,10 @@ class Demo(camera_server.CameraServer):
             x2 = int(_face_loc[2] * self.face_tracker.cam_w)
             y2 = int(_face_loc[3] * self.face_tracker.cam_h)
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-        cv2.imshow('faces', frame)
+        cv2.imshow('faces1', frame)
         cv2.waitKey(1)
 
 
 if __name__ == '__main__':
     demo = Demo(camera_address=0)
     demo.run()
-
--- configs/configs.py
+++ configs/configs.py
@@ -1,6 +1,7 @@
 import os
 
-BASE_PATH = '/'.join(os.getcwd().split('/')[:-1]) # Using ubuntu machine may require removing this -1
+BASE_PATH = '{}/tf-insightface'.format('/'.join(os.getcwd().split('/')[:-1])) # Using ubuntu machine may require removing this -1
+print("PATH= ", BASE_PATH)
 face_describer_input_tensor_names = ['img_inputs:0', 'dropout_rate:0']
 face_describer_output_tensor_names = ['resnet_v1_50/E_BN2/Identity:0']
 face_describer_device = '/cpu:0'
